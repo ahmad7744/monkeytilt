e485f87d68a6dcaca206599162126ca0cd82b8c1
unknown <ahmedaslam.alphasquad@gmail.com>
2023-09-22 21:33:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
         src="/images/Untitled_Artwork.png"
         alt=""
       />
-      <div className="w-full  max-w-[1536px]   mx-auto">
-          <div className="flex justify-between   items-center w-full ">
-            <div className="flex  z-10 flex-col">
-              <h2 className="text-white text-lg uppercase leading-7 tracking-[9px] font-extrabold">
+      <div className="w-full  max-w-[1536px] px-5  mx-auto">
+          <div className="flex flex-col md:flex-row justify-center items-center  md:justify-between  md:items-center w-full ">
+            <div className="flex text-center md:text-left  z-10 flex-col">
+              <h2 className="text-white text-sm md:text-lg uppercase leading-7 md:tracking-[9px] tracking-[6px] font-extrabold">
                 WELCOME TO CASINO LOBBY
               </h2>
-              <p className="text-white text-7xl font-['Avenir Next']  uppercase leading-[72px] tracking-widest  ">
+              <p className="text-white text-3xl md:text-7xl font-['Avenir Next']  uppercase leading-[42px] md:leading-[72px] tracking-widest  ">
                 SIDHARTH SANKHE
               </p>
             </div>
@@ -68,8 +68,8 @@ function App() {
       
         
         {/* slide bar main div */}
-        <div className="w-full flex flex-col gap-28 pt-12">
-          <div className="px-24 py-5 bg-ga justify-center items-center gap-12 flex">
+        <div className="w-full flex flex-col gap-14 md:gap-28 pt-12">
+          <div className="px-24 py-5 bg-ga justify-center items-center gap-12 flex flex-col md:flex-row flex-wrap ">
             <p className="text-center text-zinc-400 text-sm font-medium font-['Avenir Next'] uppercase leading-tight cursor-pointer">
               FAVORITES
             </p>
@@ -110,8 +110,8 @@ function App() {
               tournaments
             </p>
           </div>
-          <div className="flex max-w-[1536px] w-full justify-between mx-auto ">
-            <div className="flex flex-col gap-6 ">
+          <div className="flex flex-col md:flex-row px-5 gap-4 md:gap-0 max-w-[1536px] w-full justify-between mx-auto ">
+            <div className="flex flex-col gap-2 md:gap-6 ">
               <p className=" text-white text-[32px] font-normal font-['Mulish'] uppercase leading-10">
                 YOUR FAVORITES
               </p>
@@ -128,7 +128,7 @@ function App() {
             </div>
           </div>
           {/* permotions */}
-          <div className="flex w-full max-w-[1536px] mx-auto justify-between ">
+          <div className="flex flex-col md:flex-row px-5 gap-4  w-full max-w-[1536px] mx-auto justify-between ">
             <div className="flex flex-col justify-between max-w-[360px]  w-full ">
               <p className="text-white text-[32px] font-normal font-['Mulish'] uppercase leading-10">
                 Promotions
@@ -152,7 +152,7 @@ function App() {
             <div className="max-w-[360px] w-full">
               <img src="./images/Card-02.png" alt="" />
             </div>
-            <div className="max-w-[360px] w-full self-end">
+            <div className="max-w-[360px] w-full self-start md:self-end">
               <p className="text-zinc-400 text-sm font-normal font-['Avenir Next'] leading-tight">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt
@@ -164,9 +164,9 @@ function App() {
           </div>
           {/* Top picks section */}
           <div className="w-full max-w-[1536px]  justify-between mx-auto ">
-            <div className="w-full flex justify-center mx-auto items-center ">
+            <div className="w-full flex flex-col gap-4 px-5 md:flex-row md:gap-0 justify-center mx-auto items-center ">
               <div className="w-full flex flex-col max-w-[768px] justify-center  items-center gap-4 ">
-                <div className="w-full max-w-[384px] flex flex-col  gap-4 ">
+                <div className="w-full max-w-[384px] flex flex-col   gap-2 md:gap-4 ">
                   <p className="text-white text-[32px] font-normal font-['Mulish'] uppercase leading-10 ">
                     Top Picks for you
                   </p>
@@ -183,12 +183,12 @@ function App() {
                 <img src="./images/Card.png" alt="" />
               </div>
             </div>
-            <div className="w-full flex justify-center mx-auto items-center ">
+            <div className="w-full flex flex-col gap-4 px-5 md:flex-row md:gap-0 pt-4 md:pt-0 justify-center mx-auto items-center ">
               <div className="w-full flex flex-col max-w-[768px] justify-center items-center  ">
                 <img src="./images/Card-03.png" alt="" />
               </div>
               <div className="w-full flex flex-col max-w-[768px] justify-center  items-center  ">
-                <div className="w-full max-w-[384px] flex flex-col  gap-4 ">
+                <div className="w-full max-w-[384px] flex flex-col gap-2  md:gap-4 ">
                   <p className="text-white text-[32px] font-normal font-['Mulish'] uppercase leading-10 ">
                     Recently played
                   </p>
@@ -206,7 +206,7 @@ function App() {
           {/* More games section */}
           <div className="w-full max-w-[1536px] mx-auto flex flex-col gap-10 ">
             <hr className="border-t border-zinc-700" />
-            <div className="flex w-full max-w-[1536px] justify-between gap-10">
+            <div className="flex flex-col px-5 w-full max-w-[1536px] justify-between gap-5 md:gap-10">
               <p className="text-white text-[32px] font-normal font-['Mulish'] uppercase leading-10">
                 More Games
               </p>
@@ -215,8 +215,8 @@ function App() {
                 eiusmod tempor incididunt
               </p>
             </div>
-            <div className="grid grid-cols-3 gap-8">
-              <div className="w-full flex flex-col gap-6 ">
+            <div className="grid grid-cols-1 md:grid-cols-3 px-5 gap-8">
+              <div className="w-full flex flex-col gap-2 md:gap-6 ">
                 <img src="./images/Thumbnail-5.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   LIVE GAMES
@@ -225,7 +225,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/Thumbnail.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   ROULETTE
@@ -234,7 +234,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/BLACKJACK.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   BLACKJACK
@@ -243,7 +243,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/CHALLENGES.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   CHALLENGES
@@ -252,7 +252,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/ORIGINALS.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   ORIGINALS
@@ -261,7 +261,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/TOURNAMENTS.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   TOURNAMENTS
@@ -270,7 +270,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/BLACKJACK-2.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   BLACKJACK
@@ -279,7 +279,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/PROVIDERS.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   PROVIDERS
@@ -288,7 +288,7 @@ function App() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </p>
               </div>
-              <div className="w-full gap-6 flex flex-col ">
+              <div className="w-full  gap-2 md:gap-6 flex flex-col ">
                 <img src="./images/GAMESHOWS.png" alt="" />
                 <p className="text-white text-[28px] font-normal font-['Mulish'] uppercase leading-7">
                   GAME SHOWS
@@ -305,7 +305,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="w-full max-w-[1536px] flex justify-between mx-auto py-16">
+        <div className="w-full max-w-[1536px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-5 justify-center px-5 md:gap-0 md:justify-between mx-auto py-16">
           <div className="flex flex-col">
             <ul className="flex flex-col leading-7 gap-5 ">
               <li className="  text-white text-lg font-semibold font-['Mulish'] uppercase leading-7 tracking-widest ">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,68 +3,77 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-        
-    
-    <div className=" bg-black z-50 ">
-      <div className="w-full max-w-[1536px] mx-auto flex items-center justify-between  z-50 ">
-        <div className="w-full  max-w-[607px] z-50">
-          <ul className="space-x-8 lg:flex text-white items-center text-base font-medium leading-7 ">
-            <li className="hover:text-[#FFE500] border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] cursor-pointer ">
-              <a href="/">CASINO</a>
-            </li>
-            <li className="hover:text-[#FFE500]  cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] uppercase">
-              <a href="/Poker">Poker</a>
-            </li>
-            <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]  uppercase">
-              <a href="/SPORTSBOOK">SPORTSBOOK</a>
-            </li>
-            <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]  uppercase">
-              <a href="/CRYPTO">CRYPTO</a>
-            </li>
-            <li className="hover:text-[#FFE500] cursor-pointer  border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] uppercase">
-              <a href="/celebrities">celebrities</a>
-            </li>
-            <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]   uppercase ">
-              <a href="/SHOP">SHOP</a>
-            </li>
-          </ul>
-        </div>
-        <div className="cursor-pointer ">
-         <a href="/"><img src="/images/MonkeyTiltLogoWhite.png" alt="logo" /></a> 
-        </div>
-        <div className="max-w-[580px] flex gap-6 ">
-          <div className=" px-2 py-1 bg-white bg-opacity-5 rounded-[32px] backdrop-blur-[128px] justify-center items-center gap-1 inline-flex">
-            <p className="text-neutral-400 text-sm font-medium leading-tight">
-              VIP Status:
-            </p>{" "}
-            <span className="text-white text-sm font-medium leading-tight">
-              Bronze
-            </span>
-            <img src="/images/Frame.png" alt="" />
+      <div className=" bg-black z-50 ">
+        <div className="w-full max-w-[1536px]  mx-auto md:flex md:items-center md:justify-between  z-50 ">
+          <div className="z-50">
+            <ul className="  lg:space-x-10 lg:flex w-full  max-w-[607px] lg:justify-between hidden  text-white items-center text-base font-medium leading-7 ">
+              <li className="hover:text-[#FFE500] border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] cursor-pointer ">
+                <a href="/">CASINO</a>
+              </li>
+              <li className="hover:text-[#FFE500]  cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] uppercase">
+                <a href="/Poker">Poker</a>
+              </li>
+              <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]  uppercase">
+                <a href="/SPORTSBOOK">SPORTSBOOK</a>
+              </li>
+              <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]  uppercase">
+                <a href="/CRYPTO">CRYPTO</a>
+              </li>
+              <li className="hover:text-[#FFE500] cursor-pointer  border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500] uppercase">
+                <a href="/celebrities">celebrities</a>
+              </li>
+              <li className="hover:text-[#FFE500] cursor-pointer border-b-2 border-b-transparent transition-all duration-500 hover:border-[#FFE500]   uppercase ">
+                <a href="/SHOP">SHOP</a>
+              </li>
+            </ul>
           </div>
-          <div className=" flex items-center gap-1 px-2 py-1 bg-[#453F02] rounded-[32px]">
-            <img src="/images/Frame-2.png" alt="" />
-            <p className="text-yellow-400 text-sm font-medium uppercase leading-tight">
-              $60.89
-            </p>
+          <div></div>
+          <div className="flex flex-col items-center cursor-pointer md:flex-row  ">
+            <a href="/">
+              <img src="/images/MonkeyTiltLogoWhite.png" alt="logo" />
+            </a>
           </div>
-          <div className="flex items-center gap-6">
-            <img
-              className="cursor-pointer"
-              src="/images/search-2-line.png"
-              alt=""
-            />
-            <img
-              className="cursor-pointer"
-              src="/images/user-6-line.png"
-              alt=""
-            />
-            <img className="cursor-pointer" src="/images/Frame-3.png" alt="" />
-            <img className="cursor-pointer" src="/images/Frame-4.png" alt="" />
+          <div className="max-w-[390px] md:max-w-[580px] pb-2 flex  gap-4 md:gap-6 ">
+            <div className=" px-5 py-1 md:px-2 md:py-1 bg-white bg-opacity-5 rounded-[32px] backdrop-blur-[128px] justify-center items-center gap-1 inline-flex">
+              <p className="text-neutral-400 text-[10px] md:text-[14px] font-medium leading-tight">
+                VIP Status:
+              </p>{" "}
+              <span className="text-white text-[10px] md:text-[14px] font-medium leading-tight">
+                Bronze
+              </span>
+              <img src="/images/Frame.png" alt="" />
+            </div>
+            <div className=" flex items-center gap-1 px-2 py-1 bg-[#453F02] rounded-[32px]">
+              <img src="/images/Frame-2.png" alt="" />
+              <p className="text-yellow-400 text-[8px] md:text-[14px] font-medium uppercase leading-tight">
+                $60.89
+              </p>
+            </div>
+            <div className="flex items-center gap-4 md:gap-6">
+              <img
+                className="cursor-pointer"
+                src="/images/search-2-line.png"
+                alt=""
+              />
+              <img
+                className="cursor-pointer"
+                src="/images/user-6-line.png"
+                alt=""
+              />
+              <img
+                className="cursor-pointer"
+                src="/images/Frame-3.png"
+                alt=""
+              />
+              <img
+                className="cursor-pointer"
+                src="/images/Frame-4.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
